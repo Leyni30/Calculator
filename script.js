d0125bf9b8a0caea3a811c823170d3d07b0f7632
Elena <elena_donceva@yahoo.com>
2020-05-21 17:34:03 -0400

    added I think most of the functionality within conditionals after grabbing a hold of the divs from the DOM

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const result = document.getElementsByClassName(result);
-console.log(result);
-let buttons = document.querySelectorAll("ul li");
-console.log(buttons);
-let value = button.innerText;
+const $buttons = document.querySelectorAll("ul li");
+
+$buttons.forEach((node) => {
+    node.addEventListener('onmousedown', function (event) {
+        const value = node.innerText.trim();
+        const $result = document.querySelector('result');
+        const resultText = $result.innerText.trim();
+        if ($resultText === 0) {
+            $result.innerText = "";
+        }
+        if (value === "=") {
+            let solution = eval(resultText);
+            $result.innerText = solution;
+            return true;
+        }
+        if (value.toLowerCase() === 'c') {
+            $result.innerText = "";
+            return true;
+        }
+
+        $result.append(value);
+    })
+})
