f09f96cf48d4886f1bcf7bacff625334533441b6
Elena <elena_donceva@yahoo.com>
2020-05-21 13:21:14 -0400

    grabbed hold of the buttons

---
+++ script.js
@@ -0,0 +1,5 @@
+const result = document.getElementsByClassName(result);
+console.log(result);
+let buttons = document.querySelectorAll("ul li");
+console.log(buttons);
+let value = button.innerText;
